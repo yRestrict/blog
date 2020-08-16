d45025a42c9cdadf91d005825c07ba9aa1e1629d
yRestrict <46817750+yRestrict@users.noreply.github.com>
2020-08-16 19:35:22 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+	<meta charset="utf-8">
+	<title>Tudo sobre Counter-Strike </title>
+	<meta name="description" content="Clique aqui para obter mais informações sobre o Counter-Strike.">
+	<meta property="og:image" content="#">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#111">
+
+	<!-- CSS-->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<!-- Slide -->
+	<link rel="stylesheet" href="assets/scripts/ulightbox/ulightbox.min.css"/>
+	<script src="assets/scripts/ulightbox/ulightbox.min.js"></script>
+	
+	<!-- Slick -->
+	<link rel="stylesheet" type="text/css" href="assets/js/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="assets/js/slick/slick-theme.css">
+	
+	<!-- Aleatorios -->
+	<link rel="stylesheet" href="assets/scripts/src/base.min.css" />
+    <link rel="stylesheet" href="assets/scripts/src/layer7.min.css" />
+
+    <script src="assets/scripts/src/jquery-1.12.4.min.js"></script>
+
+    <script src="assets/scripts/src/uwnd.min.js"></script>
+
+
+
+	
+	
+	
+	
+	
+	
+	<link type="text/css" rel="stylesheet" href="assets/scripts/src/social.css" />
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i&amp;subset=cyrillic" rel="stylesheet">
+	<link rel="icon" type="image/png" href="/sites/all/themes/zen/img/favicon2.png">
+	<link rel="canonical" href="https://zplague.com.br">
+</head>
+<body>
+	<header class="header">
+		<div class="top_header">
+			<div class="container d-flex justify-content-between align-items-center">
+				<div class="h-left-side">
+					<a href="/" class="logo_top mr-auto">
+						<img src="assets/img/elements/logo.png" alt=""></img>
+					</a>
+					<div class="search_top">
+						<form id="quicksearch" method="get" action="https://zplague.com.br/search">
+							<input type="text" name="q" class="input_search" placeholder="Procurar" required="required">
+								<button class="btn_search">
+									<img src="assets/img/svg/search.svg" alt="Procura Arquivos">
+								</button>
+						</form>
+					</div> <!--FIM search_top -->
+				</div> <!--FIM left -->
+				<a href="https://url.csrevo.com/getcs" id="button_top">
+					<span class="arrow"> <span class="s1"></span> <span class="s2"></span></span>
+					<span> BAIXA CS 1.6<i>(186,3 Mb)</i></span>                
+				</a>
+				<div class="h-right-side">
+					<span class="mobail_search" onclick="$('.search_top').slideToggle(400);">
+					<img src="assets/img/svg/search.svg" alt="">
+					</span>
+					<span class="mobail_menu_btn" onclick="$('.header_nav').slideToggle(400);">
+						<span></span>
+						<span></span>
+						<span></span>
+					</span>
+				</div>
+			</div> <!--FIM container -->
+		</div> <!--FIM top_header -->
+
+		<div class="header_nav">
+			<div class="container">
+				<nav class="menu_top" itemscope itemtype="https://schema.org/SiteNavigationElement">
+					<span>
+						<a href="/" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_1"></i>
+							<span itemprop="name">Inicio</span>
+						</a>
+					</span>
+					<span>
+						<a href="https://zplague.com.br/addons" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_2"></i>
+							<span itemprop="name">Addons CS 1.6</span>
+						</a>
+					</span>
+					<span>
+						<a href="https://zplague.com.br/addons" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_3"></i>
+							<span itemprop="name">Models Armas</span>
+						</a>
+					</span>
+					<span>
+						<a href="https://zplague.com.br/maps" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_4"></i>
+							<span itemprop="name">Maps CS 1.6</span>
+						</a>
+					</span>
+					<span>
+						<a href="https://zplague.com.br/treino-de-mira" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_5"></i>
+							<span itemprop="name">Treinar Mira (mini-jogo)</span>
+						</a>
+					</span>
+					<span>
+						<a href="https://zplague.com.br/avatars" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_6"></i>
+							<span itemprop="name">Avatares</span>
+						</a>
+					</span>
+					<span>
+						<a href="https://zplague.com.br/config-cs16" itemprop="url" class="item_menu">
+							<span class="line_menu"></span>
+							<i class="ico_menu icon_menu_7"></i>
+							<span itemprop="name">Configs</span>
+						</a>
+					</span>
+				</nav>
+			</div> <!--FIM Container -->
+		</div> <!--FIM Header_Nav -->       
+	</header> <!--FIM header -->
+	<div class="main_content">
+		<div class="container">
+			<!-- Start Sidebar -->
+			<div class="sitebar" itemscope="" itemtype="https://schema.org/SiteNavigationElement">
+			<div class="close_sitebar" onclick="$('.sitebar').slideToggle(400);">
+				<span></span>
+				<span></span>
+            </div>
+            <div class="item_sitebar sitebar_link_mini">
+                <h3 class="title_sitebar">
+                    <i class="ico_num_1"></i>Principais
+                </h3> 
+                <ul>
+                    <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-rus" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -117px -145px;"></span>
+                            <span class="text_link_mini">
+							Baixar CS 1.6
+                            </span>
+                        </a>
+                    </li>
+                    <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-original" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -117px -60px;"></span>
+                            <span class="text_link_mini">
+							Addons
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-zbots" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -117px -145px;"></span>
+                            <span class="text_link_mini">
+							Command's Menus
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-butcher" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -5px -117px;"></span>
+                            <span class="text_link_mini">
+							Dicas & Ajuda
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-zombie-bots" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -5px -33px;"></span>
+                            <span class="text_link_mini">
+							Lista de programas
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-final" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -145px -33px;"></span>
+                            <span class="text_link_mini">
+							Mods
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-russkij-specnaz" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -5px -145px;"></span>
+                            <span class="text_link_mini">
+							Plugins 4Fun
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-ot-santehnika" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -5px -145px;"></span>
+                            <span class="text_link_mini">
+							Plugins ZP, ZM (Zombie Mod)
+                            </span>
+                        </a>
+					</li>
+					</li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-theamondit-v4-launcher" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -145px -88px;"></span>
+                            <span class="text_link_mini">
+							Skins
+                            </span>
+                        </a>
+                    </li>
+                        <li itemprop="name">
+                        <a itemprop="url" href="/zplague/cs16-theamondit-v4-launcher" class="link_sitebar_nav">
+                            <span class="image_link_mini" style="background-position: -61px -33px;"></span>
+                            <span class="text_link_mini">
+							Tutoriais CS
+                            </span>
+                        </a>
+                    </li>                    
+				</ul>
+						</div>
+				<div class="item_sitebar  sitebar_link_mini ">
+            		<h3 class="title_sitebar">
+                		<i class="ico_num_3"></i>Utilitarios
+					</h3>
+					<ul>
+					<li itemprop="name">
+						<a itemprop="url" href="/sborki/cs16-fess-pro" class="link_sitebar_nav">
+							<span class="image_link_mini" style="background-position: -5px -88px;"></span>
+							<span class="text_link_mini">
+							Jed's Model Viwer (Ver .mdl)
+							</span>
+						</a>
+					</li>
+					<li itemprop="name">
+						<a itemprop="url" href="/sborki/cs16-ukr-lesnik" class="link_sitebar_nav">
+							<span class="image_link_mini" style="background-position: -33px -145px;"></span>
+							<span class="text_link_mini">
+							Valve Hammer Editor
+							</span>
+						</a>
+					</li>
+					<li itemprop="name">
+						<a itemprop="url" href="/sborki/cs16-xa1t" class="link_sitebar_nav">
+							<span class="image_link_mini" style="background-position: -61px -117px;"></span>
+							<span class="text_link_mini">
+							VoiceTranscoder RC4
+							</span>
+						</a>
+					</li>
+    
+                    <li itemprop="name">
+                		<a itemprop="url" href="/sborki/cs16-uluqq" class="link_full_sitebar">
+                    		<i><img src="img/icon/ico_full.png" alt=""></i>Aleatório >
+                		</a>
+            		</li>
+					</ul>		
+		</div> <!-- Fim container --> 
+	</div> <!-- Fim Main --> 
+	<!-- End Sidebar -->
+	<div class="content_right" itemscope itemtype="https://schema.org/Article">
+		<div class="download_block">
+			<div class="content_download">
+				<div class="row align-items-center">
+					<div class="col-12 col-lg-5 col-sm-5" itemscope itemtype="https://schema.org/ImageObject">
+						<img src="assets/img/back/cs16.jpg" class="mv-100" alt="Baixe VS 1.6 gratuitamente" itemprop="url image">
+						<meta itemprop="width" content="247"/>
+						<meta itemprop="height" content="171"/>
+					</div>
+					<div class="col-12 col-lg-7 pl-lg-0 col-sm-7">
+						<h1 class="title_download_item" itemprop="headline">Baixe o CS 1.6 gratuitamente</h1>
+						<p>
+							• Versão pura do jogo <br>
+                            • Bots inteligentes com menu de configurações flexíveis <br>
+                            • Versão BR (menu, dublagem) <br>
+                            • Funciona sem problemas em qualquer versão do Windows <br>
+                            • Atualização oficial do Steam mais recente
+						</p>
+					</div>
+				</div> <!-- row align-items-center-->
+				<div class="row">
+					<div class="col-12 col-lg-4">
+						<a href="torrents/cs16_original.torrent" onclick="$('.blockModal').show(); yaCounter50112571.reachGoal('yaMetYDDwn'); return true;" class="btn_down_1">
+							Baixar via torrent
+						</a>
+					</div>
+					<div class="col-12 col-lg-4">
+						<a href="https://cspromogame.ru/cs16_classic.exe" onclick="$('.blockModal').show(); yaCounter50112571.reachGoal('yaMetYDDwn'); return true;" class="btn_down_3">
+							Baixar por link direto
+						</a>
+					</div>
+					<div class="col-12 col-lg-4">
+						<a href="https://cloud.mail.ru/public/Ajcf/GjuoMsQSR" target="_blank" onclick="$('.blockModal').show(); yaCounter50112571.reachGoal('yaMetYDDwn'); return true;" class="btn_down_2">
+							Baixar via Media 
+						</a>
+					</div>
+				</div> <!-- row-->				
+			</div> <!-- fim content_download-->
+			<div class="content_slide">
+				<div class="box_slide">
+					<div class="item_slide_skins item_slide_skins slick-slide slick-cloned slick-active">
+						<a href="assets/img/home-slide/cs/1.jpg" class="ulightbox" data-fancybox-group="gallery">
+							<div class="image_skins_slide">
+								<img src="assets/img/home-slide/cs/1-min.jpg" width="207" class="mw-100" alt="Counter-Strike 1.6 Counter-Strike 1.6 totalmente em ingles">
+							</div>
+						</a> 						
+					</div>
+					<div class="item_slide_skins">
+						<a href="assets/img/home-slide/cs/2.jpg" class="ulightbox" data-fancybox-group="gallery">
+							<div class="image_skins_slide">
+								<img src="assets/img/home-slide/cs/2-min.jpg" width="207" class="mw-100" alt="Bots personalizáveis">
+							</div>
+						</a>
+					</div>
+					<div class="item_slide_skins">
+						<a href="assets/img/home-slide/cs/3.jpg" class="ulightbox" data-fancybox-group="gallery">
+							<div class="image_skins_slide">
+								<img src="assets/img/home-slide/cs/3-min.jpg" width="207" class="mw-100" alt="Modelos Terroristas Padrão CS 1.6">
+							</div>
+						</a>
+					</div>
+					<div class="item_slide_skins">
+						<a href="assets/img/home-slide/cs/4.jpg" class="ulightbox" data-fancybox-group="gallery">
+							<div class="image_skins_slide">
+								<img src="assets/img/home-slide/cs/4-min.jpg" width="207" class="mw-100" alt="Modelos padrão de combate ao terrorismo CS 1.6">
+							</div>
+						</a>
+					</div>
+					<div class="item_slide_skins">
+						<a href="assets/img/home-slide/cs/5.jpg" class="ulightbox" data-fancybox-group="gallery">
+							<div class="image_skins_slide">
+								<img src="assets/img/home-slide/cs/5-min.jpg" width="207" class="mw-100" alt="Armas padrão do Counter Strike 1.6">
+							</div>
+						</a>
+					</div> 					
+				</div> <!--fim box_slide-->
+			</div> <!--fim content_slide-->						
+		</div> <!-- end download_block-->
+		
+	</div> <!-- content_right fim-->
+
+	
+			
+
+
+						
+    <script src="assets/js/slick/slick.min.js"></script>
+    <script src="assets/js/common.js"></script>
+    <script>
+        $('.box_slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            centerMode: false,
+            arrows: true,
+            responsive: [
+
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+    </script>
+
+</body>
+</html>
